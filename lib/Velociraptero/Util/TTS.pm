2fa0978c54c568d5149a6955aa9fea1d6fd443a8
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-05-03 05:38:23 -0500

    fix TTS preprocessing

--- lib/Velociraptero/Util/TTS.pm
+++ lib/Velociraptero/Util/TTS.pm
@@ -10,9 +10,9 @@ sub preprocess_for_tts {
 	$_ = $text;
 	$_ = unidecode($_); # FIXME this is a sledgehammer approach
 
-	s/\[(\d+(,\s*\d+)*)\]/citation \1/gi; # [12,28] -> citations 12, 28
-	s/Fig[. ]*(\d+)/Figure \1/gi; # Fig. 4 -> Figure 4
-	s/Sec[. ]*(\d+)/Section \1/gi; # Sec. 2 -> Section 2
+	s/\[(\d+(,\s*\d+)*)\]/citation $1/gi; # [12,28] -> citations 12, 28
+	s/Fig[. ]*(\d+)/Figure $1/gi; # Fig. 4 -> Figure 4
+	s/Sec[. ]*(\d+)/Section $1/gi; # Sec. 2 -> Section 2
 	s/e\.g\.,/for example,/gi; # (e.g., text) -> (for example, text)
 	s/i\.e\.,/that is,/gi; # (i.e., text) -> (that is, text)
 	$_;
